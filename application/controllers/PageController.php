d8da46880be01e7d3a4280daa1cae21ba7ca4680
Rin Minase <rin.black.raison.detre@gmail.com>
2017-09-05 21:45:19 +0800

    ADD: renamed 'new' to add

--- application/controllers/PageController.php
+++ application/controllers/PageController.php
@@ -5,5 +5,5 @@ class PageController extends CI_Controller {
 	public function __construct(){ parent::__construct(); }
 
 	public function index()	{ $this->load->view('index'); }
-	public function create() { $this->load->view('new'); }
+	public function create() { $this->load->view('add'); }
 }
