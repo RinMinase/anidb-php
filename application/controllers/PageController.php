934a02330df9e843a4371692a4245724dda03f55
Rin Minase <rin.black.raison.detre@gmail.com>
2017-09-05 14:43:26 +0800

    PAGE_CONTROLLER: modified from original 'list' file to 'index' file for easier understanding

--- application/controllers/PageController.php
+++ application/controllers/PageController.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class PageController extends CI_Controller {
 	public function __construct(){ parent::__construct(); }
 
-	public function index()	{ $this->load->view('list'); }
+	public function index()	{ $this->load->view('index'); }
 	public function create() { $this->load->view('new'); }
 }
