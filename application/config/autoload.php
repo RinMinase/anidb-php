5c30b3ee96300348833c4be578969e280f90e79d
Rin Minase <rin.black.raison.detre@gmail.com>
2017-09-07 22:51:36 +0800

    CONFIG: added database settings

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $autoload['packages'] = array();
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('database');
 $autoload['drivers'] = array();
 $autoload['helper'] = array('url');
 $autoload['config'] = array();
