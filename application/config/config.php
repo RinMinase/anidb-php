5eb8b754dbcf4210033e2038dc5cc8a255107c05
Rin Minase <rin.black.raison.detre@gmail.com>
2017-09-07 23:16:07 +0800

    CONFIG: appended default timezone to fix warnings being created

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+date_default_timezone_set("Asia/Manila");
 
 $config['base_url'] = 'http://localhost/anidb/';
 $config['index_page'] = '';
