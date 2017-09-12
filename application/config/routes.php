e498e0df2576b2193d6ee84a8b73f076967ac6a4
Rin Minase <rin.black.raison.detre@gmail.com>
2017-09-13 00:31:00 +0800

    ADD: removed unused routes from add page

--- application/config/routes.php
+++ application/config/routes.php
@@ -5,8 +5,6 @@ $route['default_controller'] = 'PageController';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-$route['new'] = 'PageController/create';
-$route['create'] = 'PageController/create';
 $route['add'] = 'PageController/create';
 $route['addEntry'] = 'DefaultController/addEntry';
 
