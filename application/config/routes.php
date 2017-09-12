5b80007b0eb5403c7d2f25b9ecb48ed34f9a993b
Rin Minase <rin.black.raison.detre@gmail.com>
2017-09-13 00:30:15 +0800

    LANDING: removed unused routes from landing page

--- application/config/routes.php
+++ application/config/routes.php
@@ -5,9 +5,6 @@ $route['default_controller'] = 'PageController';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-$route['list'] = 'PageController';
-$route['view'] = 'PageController';
-
 $route['new'] = 'PageController/create';
 $route['create'] = 'PageController/create';
 $route['add'] = 'PageController/create';
