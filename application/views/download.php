c8fd7b51c5f4583fab22ec1982d0fb9109a51757
Rin Minase <rin.black.raison.detre@gmail.com>
2017-09-16 03:24:41 +0800

    DOWNLOAD: corrected css and javascript source locations

--- application/views/download.php
+++ application/views/download.php
@@ -5,8 +5,8 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-		<link rel="stylesheet" href="resources/bootstrap-3.3.7/bootstrap.min.css">
-		<link rel="stylesheet" href="resources/font-awesome-4.7.0/font-awesome.min.css">
+		<link rel="stylesheet" href=<?php echo base_url("resources/bootstrap-3.3.7/bootstrap.min.css") ?>>
+		<link rel="stylesheet" href=<?php echo base_url("resources/font-awesome-4.7.0/font-awesome.min.css") ?>>
 
 		<style>
 
@@ -349,8 +349,8 @@
 
 		</div>
 
-		<script src="resources/jquery-3.1.1/jquery-3.1.1.min.js"></script>
-		<script src="resources/bootstrap-3.3.7/bootstrap.min.js"></script>
+		<script src=<?php echo base_url("resources/jquery-3.1.1/jquery-3.1.1.min.js") ?>></script>
+		<script src=<?php echo base_url("resources/bootstrap-3.3.7/bootstrap.min.js") ?>></script>
 
 		<script>
 			$(document).ready(function(){
