10f05af070dae411ef6dd21febf26a77455e0534
Rin Minase <rin.black.raison.detre@gmail.com>
2017-09-16 03:20:29 +0800

    ROUTES: added routes for other lists, download list, hdd list and hdd simulator

--- application/config/routes.php
+++ application/config/routes.php
@@ -8,4 +8,11 @@ $route['translate_uri_dashes'] = FALSE;
 $route['add'] = 'PageController/create';
 $route['addEntry'] = 'DefaultController/addEntry';
 
+$route['other-lists'] = 'PageController/other_lists';
+
+$route['download-list'] = 'PageController/download_list';
+
+$route['hdd-list'] = 'PageController/hdd_list';
+$route['hdd-list/simulator'] = 'PageController/hdd_simulator';
+
 $route['about'] = 'PageController/about';
--- application/controllers/PageController.php
+++ application/controllers/PageController.php
@@ -12,5 +12,14 @@ class PageController extends CI_Controller {
 	}
 
 	public function create() { $this->load->view('add'); }
+
+	public function other_lists() { $this->load->view('other-lists'); }
+
+	public function download_list() { $this->load->view('download'); }
+
+	public function hdd_list() { $this->load->view('hdd'); }
+
+	public function hdd_simulator() { $this->load->view('hdd-simulator'); }
+
 	public function about() { $this->load->view('about'); }
 }
