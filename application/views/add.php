2cad2d3ba0b77ef5e917bbca3b794cb00bcad7cb
Rin Minase <rin.black.raison.detre@gmail.com>
2017-09-18 02:46:08 +0800

    ADD: removed uneeded stylesheet

--- application/views/add.php
+++ application/views/add.php
@@ -7,7 +7,6 @@
 
 		<link rel="stylesheet" href=<?php echo base_url("resources/bootstrap-3.3.7/bootstrap.min.css") ?>>
 		<link rel="stylesheet" href=<?php echo base_url("resources/font-awesome-4.7.0/font-awesome.min.css") ?>>
-		<link rel="stylesheet" href=<?php echo base_url("resources/css/styles.css") ?>>
 
 		<style>
 
