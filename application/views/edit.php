e176e91305819a481d087084ace7cda507bbc98b
Rin Minase <rin.black.raison.detre@gmail.com>
2017-09-20 05:10:39 +0800

    EDIT: added edit module

---
+++ application/views/edit.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+		<link rel="stylesheet" href=<?php echo base_url("resources/bootstrap-3.3.7/bootstrap.min.css") ?>>
+		<link rel="stylesheet" href=<?php echo base_url("resources/font-awesome-4.7.0/font-awesome.min.css") ?>>
+
+		<style>
+
+		</style>
+
+		<title>Edit</title>
+	</head>
+	<body>
+
+		<div class="container">
+
+		</div>
+
+		<script src=<?php echo base_url("resources/jquery-3.1.1/jquery-3.1.1.min.js") ?>></script>
+		<script src=<?php echo base_url("resources/bootstrap-3.3.7/bootstrap.min.js") ?>></script>
+
+	</body>
+</html>
