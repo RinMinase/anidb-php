4cf112071a6cfdaa4a7e962dd58a6b251efc8cf3
Rin Minase <rin.black.raison.detre@gmail.com>
2017-09-20 05:52:52 +0800

    ADD: added responsive layout for smaller screens

--- application/views/add.php
+++ application/views/add.php
@@ -31,23 +31,34 @@
 					<?php echo form_open('addEntry', array('method' => 'GET')); ?>
 
 						<div class="row">
-							<div class="col-md-3 form-group">
+							<div class="col-xs-5 col-sm-3 form-group">
 								<label>Quality</label>
 								<select class="form-control" name="quality">
 									<option>4k 2160p</option>
-									<option selected>FHD 1080p</option>
+									<option>FHD 1080p</option>
 									<option>HD 720p</option>
 									<option>HQ 480p</option>
 									<option>LQ 480p</option>
 								</select>
 							</div>
 
-							<div class="col-md-6 form-group">
+							<div class="col-xs-7 form-group visible-xs hidden-sm hidden-md hidden-lg">
+								<label>Status</label>
+								<select class="form-control" name="watchStatus">
+									<option value="0" selected>Finished Watching</option>
+									<option value="1">Currently Watching</option>
+									<option value="2">Not Watched</option>
+									<option value="3">For Downloading</option>
+									<option value="4">Dropped</option>
+								</select>
+							</div>
+
+							<div class="col-xs-12 col-sm-9 col-md-6 form-group">
 								<label>Title</label>
 								<input type="text" class="form-control" name="title" required>
 							</div>
 
-							<div class="col-md-3 form-group">
+							<div class="col-sm-12 col-md-3 form-group hidden-xs">
 								<label>Status</label>
 								<select class="form-control" name="watchStatus">
 									<option value="0" selected>Finished Watching</option>
@@ -60,44 +71,48 @@
 						</div>
 
 						<div class="row">
-							<div class="col-md-2 form-group">
+							<div class="col-xs-4 col-sm-2 form-group">
 								<label>Episodes</label>
 								<input type="text" class="form-control" name="episodes">
 							</div>
-							<div class="col-md-2 form-group">
+							<div class="col-xs-4 col-sm-2 form-group">
 								<label>OVAs</label>
 								<input type="text" class="form-control" name="ovas">
 							</div>
-							<div class="col-md-2 form-group">
+							<div class="col-xs-4 col-sm-2 form-group">
 								<label>Specials</label>
 								<input type="text" class="form-control" name="specials">
 							</div>
-							<div class="col-md-3 form-group">
+							<div class="col-sm-3 form-group">
 								<label>Date Finished</label>
 								<input type="date" class="form-control" name="dateFinished">
 							</div>
-							<div class="col-md-3 form-group">
+							<div class="col-sm-3 form-group">
 								<label>File Size</label>
 								<input type="text" class="form-control" name="filesize">
 							</div>
 						</div>
 
 						<div class="row">
-							<div class="col-md-4 form-group">
+							<div class="col-sm-4 form-group hidden-xs">
 								<label>Season # (N/A if not applicable)</label>
 								<input type="text" class="form-control" name="seasonNumber">
 							</div>
-							<div class="col-md-8 form-group">
+							<div class="col-xs-3 form-group visible-xs hidden-sm hidden-md hidden-lg">
+								<label>Season</label>
+								<input type="text" class="form-control" name="seasonNumber">
+							</div>
+							<div class="col-xs-9 col-sm-8 form-group">
 								<label>First Season Title</label>
 								<input type="text" class="form-control" name="title">
 							</div>
 						</div>
 
 						<div class="row">
-							<div class="col-md-5 form-group">
+							<div class="col-xs-12 col-sm-6 col-md-5 form-group">
 								<label>Season Released</label>
 								<div class="row">
-									<div class="col-md-8">
+									<div class="col-xs-6 col-sm-8">
 										<select class="form-control" name="releaseSeason">
 											<option value="Winter">Winter</option>
 											<option value="Spring">Spring</option>
@@ -105,7 +120,7 @@
 											<option value="Fall">Fall</option>
 										</select>
 									</div>
-									<div class="col-md-4">
+									<div class="col-xs-6 col-sm-4">
 										<select class="form-control" name="releaseYear">
 											<option>2010</option>
 											<option>2011</option>
@@ -120,7 +135,7 @@
 								</div>
 							</div>
 
-							<div class="col-md-2 form-group">
+							<div class="col-xs-4 col-sm-6 col-md-2 form-group">
 								<label>Duration</label>
 								<input type="text"
 									class="form-control text-center"
@@ -128,7 +143,7 @@
 									value="00:00:00">
 							</div>
 
-							<div class="col-md-5 form-group">
+							<div class="col-xs-8 col-sm-12 col-md-5 form-group">
 								<label>Subber / Encoder</label>
 								<input type="text" class="form-control" name="encoder">
 							</div>
@@ -146,7 +161,7 @@
 						</div>
 
 						<div class="row">
-							<div class="col-md-4">
+							<div class="col-sm-6 col-sm-offset-6 col-md-4">
 								<div class="form-group">
 									<button class="btn btn-primary btn-block">Submit</button>
 								</div>
