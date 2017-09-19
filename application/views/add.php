dccd53eb22f1b72363b9eed15efe499d47abd36d
Rin Minase <rin.black.raison.detre@gmail.com>
2017-09-20 06:13:23 +0800

    ADD: fixed offset issue on submit button on desktop screen size

--- application/views/add.php
+++ application/views/add.php
@@ -161,7 +161,7 @@
 						</div>
 
 						<div class="row">
-							<div class="col-xs-12 col-sm-6 col-sm-offset-6 col-md-4">
+							<div class="col-xs-12 col-sm-6 col-sm-offset-6 col-md-4 col-md-offset-0">
 								<div class="form-group">
 									<button class="btn btn-primary btn-block">Submit</button>
 								</div>
