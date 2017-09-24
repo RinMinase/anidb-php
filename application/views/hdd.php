247823b270119e11045905cdebb20ddd2c167e0a
Rin Minase <rin.black.raison.detre@gmail.com>
2017-09-24 15:28:14 +0800

    HDD: removed header and footer code

--- application/views/hdd.php
+++ application/views/hdd.php
@@ -1,199 +1,174 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<div class="container-fluid">
+	<div class="page-header">
+		<h2>Disk List</h2>
+		<a href=<?php echo base_url() ?> class="btn btn-primary">
+			<i class="fa fa-chevron-left"></i>&nbsp;&nbsp;Back
+		</a>
+		<a href=<?php echo base_url("hdd-list/simulator") ?> class="btn btn-primary">
+			<i class="fa fa-flask"></i>&nbsp;&nbsp;Disk Simulator
+		</a>
+	</div>
 
-		<link rel="stylesheet" href=<?php echo base_url("resources/bootstrap-3.3.7/bootstrap.min.css") ?>>
-		<link rel="stylesheet" href=<?php echo base_url("resources/font-awesome-4.7.0/font-awesome.min.css") ?>>
-		<link rel="stylesheet" href=<?php echo base_url("resources/css/styles.css") ?>>
+	<div class="row">
+		<div class="col-md-3">
+			<div class="panel panel-green">
+				<div class="panel-heading">
+					<h4>Disks</h4>
+				</div>
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-md-12">
 
-		<style>
-
-		</style>
-
-		<title>Disk List</title>
-	</head>
-	<body>
-
-		<div class="container-fluid">
-			<div class="page-header">
-				<h2>Disk List</h2>
-				<a href=<?php echo base_url() ?> class="btn btn-primary">
-					<i class="fa fa-chevron-left"></i>&nbsp;&nbsp;Back
-				</a>
-				<a href=<?php echo base_url("hdd-list/simulator") ?> class="btn btn-primary">
-					<i class="fa fa-flask"></i>&nbsp;&nbsp;Disk Simulator
-				</a>
-			</div>
-
-			<div class="row">
-				<div class="col-md-3">
-					<div class="panel panel-green">
-						<div class="panel-heading">
-							<h4>Disks</h4>
-						</div>
-						<div class="panel-body">
 							<div class="row">
-								<div class="col-md-12">
-
-									<div class="row">
-										<div class="col-xs-2 col-sm-2 col-md-4 col-lg-4">
-											<p><strong>A - I</strong></p>
-										</div>
-										<div class="col-xs-10 col-sm-10 col-md-8 col-lg-8">
-											<div class="progress">
-												<div class="progress-bar progress-bar-success" style="width:50%">50%</div>
-											</div>
-										</div>
+								<div class="col-xs-2 col-sm-2 col-md-4 col-lg-4">
+									<p><strong>A - I</strong></p>
+								</div>
+								<div class="col-xs-10 col-sm-10 col-md-8 col-lg-8">
+									<div class="progress">
+										<div class="progress-bar progress-bar-success" style="width:50%">50%</div>
 									</div>
+								</div>
+							</div>
 
-									<div class="row">
-										<div class="col-xs-2 col-sm-2 col-md-4 col-lg-4">
-											<p><strong>J - Q</strong></p>
-										</div>
-										<div class="col-xs-10 col-sm-10 col-md-8 col-lg-8">
-											<div class="progress">
-												<div class="progress-bar progress-bar-warning" style="width:80%">80%</div>
-											</div>
-										</div>
+							<div class="row">
+								<div class="col-xs-2 col-sm-2 col-md-4 col-lg-4">
+									<p><strong>J - Q</strong></p>
+								</div>
+								<div class="col-xs-10 col-sm-10 col-md-8 col-lg-8">
+									<div class="progress">
+										<div class="progress-bar progress-bar-warning" style="width:80%">80%</div>
 									</div>
+								</div>
+							</div>
 
-									<div class="row">
-										<div class="col-xs-2 col-sm-2 col-md-4 col-lg-4">
-											<p><strong>R - Z</strong></p>
-										</div>
-										<div class="col-xs-10 col-sm-10 col-md-8 col-lg-8">
-											<div class="progress">
-												<div class="progress-bar progress-bar-danger" style="width:90%">90%</div>
-											</div>
-										</div>
+							<div class="row">
+								<div class="col-xs-2 col-sm-2 col-md-4 col-lg-4">
+									<p><strong>R - Z</strong></p>
+								</div>
+								<div class="col-xs-10 col-sm-10 col-md-8 col-lg-8">
+									<div class="progress">
+										<div class="progress-bar progress-bar-danger" style="width:90%">90%</div>
 									</div>
-
 								</div>
 							</div>
 
 						</div>
 					</div>
 
-
 				</div>
-
-				<div class="col-md-9">
-					<div class="panel-group">
-
-						<div class="panel panel-default">
-							<div class="panel-heading">
-								<h4 class="panel-title">
-									<a href="#panel-ai" data-toggle="collapse">A - I</a>
-									<div class="pull-right">
-										<span class="badge">75</span>
-										<span>&nbsp;00.00 GB</span>
-									</div>
-								</h4>
-							</div>
-							<div id="panel-ai" class="panel-collapse collapse">
-								<div class="panel-body">
-
-									<div class="table-responsive">
-										<table class="table">
-											<thead>
-												<tr>
-													<th>Title</th>
-													<th>File Size</th>
-												</tr>
-											</thead>
-											<tbody>
-												<tr>
-													<td>Sample</td>
-													<td>Sample</td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-
-								</div>
-							</div>
-						</div>
-
-						<div class="panel panel-default">
-							<div class="panel-heading">
-								<h4 class="panel-title">
-									<a href="#panel-jq" data-toggle="collapse">J - Q</a>
-									<div class="pull-right">
-										<span class="badge">92</span>
-										<span>&nbsp;00.00 GB</span>
-									</div>
-								</h4>
-							</div>
-							<div id="panel-jq" class="panel-collapse collapse">
-								<div class="panel-body">
-
-									<div class="table-responsive">
-										<table class="table table-condensed">
-											<thead>
-												<tr>
-													<th>Title</th>
-													<th>File Size</th>
-												</tr>
-											</thead>
-											<tbody>
-												<tr>
-													<td>Sample</td>
-													<td>Sample</td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-
-								</div>
-							</div>
-						</div>
-
-						<div class="panel panel-default">
-							<div class="panel-heading">
-								<h4 class="panel-title">
-									<a href="#panel-rz" data-toggle="collapse">R - Z</a>
-									<div class="pull-right">
-										<span class="badge">107</span>
-										<span>&nbsp;00.00 GB</span>
-									</div>
-								</h4>
-							</div>
-							<div id="panel-rz" class="panel-collapse collapse">
-								<div class="panel-body">
-
-									<div class="table-responsive">
-										<table class="table table-condensed">
-											<thead>
-												<tr>
-													<th>Title</th>
-													<th>File Size</th>
-												</tr>
-											</thead>
-											<tbody>
-												<tr>
-													<td>Sample</td>
-													<td>Sample</td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-
-								</div>
-							</div>
-						</div>
-
-					</div>
-				</div>
-
 			</div>
+
 
 		</div>
 
-		<script src=<?php echo base_url("resources/jquery-3.1.1/jquery-3.1.1.min.js") ?>></script>
-		<script src=<?php echo base_url("resources/bootstrap-3.3.7/bootstrap.min.js") ?>></script>
+		<div class="col-md-9">
+			<div class="panel-group">
 
-	</body>
-</html>
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4 class="panel-title">
+							<a href="#panel-ai" data-toggle="collapse">A - I</a>
+							<div class="pull-right">
+								<span class="badge">75</span>
+								<span>&nbsp;00.00 GB</span>
+							</div>
+						</h4>
+					</div>
+					<div id="panel-ai" class="panel-collapse collapse">
+						<div class="panel-body">
+
+							<div class="table-responsive">
+								<table class="table">
+									<thead>
+										<tr>
+											<th>Title</th>
+											<th>File Size</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>Sample</td>
+											<td>Sample</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+
+						</div>
+					</div>
+				</div>
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4 class="panel-title">
+							<a href="#panel-jq" data-toggle="collapse">J - Q</a>
+							<div class="pull-right">
+								<span class="badge">92</span>
+								<span>&nbsp;00.00 GB</span>
+							</div>
+						</h4>
+					</div>
+					<div id="panel-jq" class="panel-collapse collapse">
+						<div class="panel-body">
+
+							<div class="table-responsive">
+								<table class="table table-condensed">
+									<thead>
+										<tr>
+											<th>Title</th>
+											<th>File Size</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>Sample</td>
+											<td>Sample</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+
+						</div>
+					</div>
+				</div>
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4 class="panel-title">
+							<a href="#panel-rz" data-toggle="collapse">R - Z</a>
+							<div class="pull-right">
+								<span class="badge">107</span>
+								<span>&nbsp;00.00 GB</span>
+							</div>
+						</h4>
+					</div>
+					<div id="panel-rz" class="panel-collapse collapse">
+						<div class="panel-body">
+
+							<div class="table-responsive">
+								<table class="table table-condensed">
+									<thead>
+										<tr>
+											<th>Title</th>
+											<th>File Size</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>Sample</td>
+											<td>Sample</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+	</div>
+
+</div>
