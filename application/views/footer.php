561141da7266e1bfc603566a2a7ecc6a679e27a2
Rin Minase <rin.black.raison.detre@gmail.com>
2017-09-24 14:41:53 +0800

    FOOTER: fixed footer indentation

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,13 +1,13 @@
 
-	<script src=<?php echo base_url("resources/jquery-3.1.1/jquery-3.1.1.min.js") ?>></script>
-	<script src=<?php echo base_url("resources/bootstrap-3.3.7/bootstrap.min.js") ?>></script>
-	<script src=<?php echo base_url("resources/js/scripts.js") ?>></script>
+		<script src=<?php echo base_url("resources/jquery-3.1.1/jquery-3.1.1.min.js") ?>></script>
+		<script src=<?php echo base_url("resources/bootstrap-3.3.7/bootstrap.min.js") ?>></script>
+		<script src=<?php echo base_url("resources/js/scripts.js") ?>></script>
 
-	<?php
-		if(!empty($customJS)) {
-			echo "<script src='<?php echo base_url(" . $customJS . ") ?>'></script>";
-		}
-	?>
+		<?php
+			if(!empty($customJS)) {
+				echo "<script src='<?php echo base_url(" . $customJS . ") ?>'></script>";
+			}
+		?>
 
 	</body>
 </html>
