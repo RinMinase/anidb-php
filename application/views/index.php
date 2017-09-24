8b240c5df6240ca10d68137e2a4723702f1cd564
Rin Minase <rin.black.raison.detre@gmail.com>
2017-09-24 15:26:26 +0800

    LANDING: removed scripts in footer due to updating the footer code

--- application/views/index.php
+++ application/views/index.php
@@ -186,11 +186,3 @@
 	</div>
 
 </div>
-
-<script src=<?php echo base_url("resources/jquery-3.1.1/jquery-3.1.1.min.js") ?>></script>
-<script src=<?php echo base_url("resources/bootstrap-3.3.7/bootstrap.min.js") ?>></script>
-<script src=<?php echo base_url("resources/js/scripts.js") ?>></script>
-
-<script>
-
-</script>
