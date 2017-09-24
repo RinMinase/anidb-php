0bc55c44cce5a3602979995e9a2d8c0a229f146a
Rin Minase <rin.black.raison.detre@gmail.com>
2017-09-24 12:00:12 +0800

    FOOTER: removed scripts in footer in case there would be custom scripts for a module written after 'scripts.js'

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,2 @@
-		<script src=<?php echo base_url("resources/jquery-3.1.1/jquery-3.1.1.min.js") ?>></script>
-		<script src=<?php echo base_url("resources/bootstrap-3.3.7/bootstrap.min.js") ?>></script>
-		<script src=<?php echo base_url("resources/js/scripts.js") ?>></script>
-
 	</body>
 </html>
