e227de33e040d7b02accc33cc7786fa9646a47b3
Rin Minase <rin.black.raison.detre@gmail.com>
2017-10-02 00:02:03 +0800

    ANIDB: renamed page controller to landing page controller

--- application/controllers/LandingPageController.php
+++ application/controllers/LandingPageController.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class PageController extends CI_Controller {
+class LandingPageController extends CI_Controller {
 
 	public function index()	{
 		$data['query'] = $this->input->get('q');
