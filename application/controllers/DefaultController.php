8cd9889c3adc1a88855dc1762bab2abe3c93eac4
Rin Minase <rin.black.raison.detre@gmail.com>
2017-10-02 00:09:13 +0800

    ANIDB: reduced controller code length (89 lines to 51 lines)

--- application/controllers/DefaultController.php
+++ application/controllers/DefaultController.php
@@ -8,58 +8,29 @@ class DefaultController extends CI_Controller {
 		$data['quality'] = $this->input->get('quality');
 		$data['title'] = $this->input->get('title');
 
-		if ($this->input->get('episodes')) {
-			$data['episodes'] = $this->input->get('episodes');
-		}
+		if ($this->input->get('episodes')) $data['episodes'] = $this->input->get('episodes');
+		if ($this->input->get('ovas')) $data['ovas'] = $this->input->get('ovas');
+		if ($this->input->get('specials')) $data['specials'] = $this->input->get('specials');
 
-		if ($this->input->get('ovas')) {
-			$data['ovas'] = $this->input->get('ovas');
-		}
-
-		if ($this->input->get('specials')) {
-			$data['specials'] = $this->input->get('specials');
-		}
-
-		if ($this->input->get('seasonNumber')) {
-			$data['seasonNumber'] = $this->input->get('seasonNumber');
-		}
-
-		if ($this->input->get('dateFinished')) {
-			$data['dateFinished'] = $this->input->get('dateFinished');
-		}
+		if ($this->input->get('seasonNumber')) $data['seasonNumber'] = $this->input->get('seasonNumber');
+		if ($this->input->get('dateFinished')) $data['dateFinished'] = $this->input->get('dateFinished');
 
 		if ($oldFilesize = $this->input->get('filesize')) {
 
 			for ($i=0, $j=0; $i < strlen($oldFilesize); $i++) {
 				switch ($oldFilesize[$i]) {
-					case '0':
-					case '1':
-					case '2':
-					case '3':
-					case '4':
-					case '5':
-					case '6':
-					case '7':
-					case '8':
-					case '9':
+					case '0':	case '1':	case '2':	case '3':	case '4':
+					case '5':	case '6':	case '7':	case '8':	case '9':
 						$newFilesize[$j++] = $oldFilesize[$i];
-						break;
+					break;
 				}
 			}
-
-			// echo $oldFilesize;
-			// echo join($newFilesize);
 
 			$data['filesize'] = join($newFilesize);
 		}
 
-		if ($this->input->get('releaseSeason')) {
-			$data['releaseSeason'] = $this->input->get('releaseSeason');
-		}
-
-		if ($this->input->get('releaseYear')) {
-			$data['releaseYear'] = $this->input->get('releaseYear');
-		}
+		if ($this->input->get('releaseSeason')) $data['releaseSeason'] = $this->input->get('releaseSeason');
+		if ($this->input->get('releaseYear')) $data['releaseYear'] = $this->input->get('releaseYear');
 
 		if ($this->input->get('duration')) {
 			$data['durationHour'] = explode(":", $this->input->get('duration'))[0];
@@ -67,19 +38,10 @@ class DefaultController extends CI_Controller {
 			$data['durationSecond'] = explode(":", $this->input->get('duration'))[2];
 		}
 
-		if ($this->input->get('encoder')) {
-			$data['encoder'] = $this->input->get('encoder');
-		}
-		if ($this->input->get('variants')) {
-			$data['variants'] = $this->input->get('variants');
-		}
-		if ($this->input->get('remarks')) {
-			$data['remarks'] = $this->input->get('remarks');
-		}
+		if ($this->input->get('encoder')) $data['encoder'] = $this->input->get('encoder');
+		if ($this->input->get('variants')) $data['variants'] = $this->input->get('variants');
+		if ($this->input->get('remarks')) $data['remarks'] = $this->input->get('remarks');
 
-		// print_r($data);
-
-		// $this->load->model('defaultmodel');
 		// $this->defaultmodel->addAnimeData($data);
 
 		redirect("/");
