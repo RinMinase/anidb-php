7ffd8129b43714d6c4f4b9ece74931c0dfd00d78
Rin Minase <rin.black.raison.detre@gmail.com>
2017-10-01 23:59:31 +0800

    ANIDB: added general pages controller, removed general pages code in page controller

--- application/controllers/PageController.php
+++ application/controllers/PageController.php
@@ -54,49 +54,4 @@ class PageController extends CI_Controller {
 		$this->displaylibrary->display_page('add', $navbar, $data);
 	}
 
-	public function about() {
-		$raw_data = $this->defaultmodel->getAnimeStatisticsNeededData();
-
-		$total_hours = $total_minutes = $total_seconds = 0;
-		$total_filesize = 0;
-		$data['totalEpisodes'] = 0;
-		$data['totalUHD'] = $data['totalFHD'] = $data['totalHD'] = 0;
-		$data['totalLQ'] = $data['totalHQ'] = 0;
-
-		foreach ($raw_data as $item) {
-			$total_hours += $item->durationHour;
-			$total_minutes += $item->durationMinute;
-			$total_seconds += $item->durationSecond;
-
-			$total_filesize += $item->filesize;
-			$data['totalEpisodes'] += $item->episodes + $item->ovas + $item->specials;
-
-			switch ($item->quality) {
-				case "4K 2160p": $data['totalUHD']++; break;
-				case "FHD 1080p": $data['totalFHD']++; break;
-				case "HD 720p": $data['totalHD']++; break;
-				case "HQ 480p": $data['totalHQ']++; break;
-				case "LQ 360p": $data['totalLQ']++; break;
-			}
-
-		}
-
-		$reference_date = new DateTime('2000-01-01');
-		$watch_total = $reference_date->setTime($total_hours, $total_minutes, $total_seconds);
-
-		$data['totalWatchDays'] = $watch_total->format("j") - 1;
-		$data['totalWatchHours'] = $watch_total->format("G");
-		$data['totalWatchMinutes'] = $watch_total->format("i");
-		$data['totalWatchSeconds'] = $watch_total->format("s");
-
-		$data['totalSizeGB'] = round($total_filesize / 1073741824, 2);
-		$data['totalSizeTB'] = round($total_filesize / 1099511627776, 2);
-
-		$navbar['activePage'] = "about";
-		$navbar['customTitle'] = "About Page";
-		$navbar['customCSS'] = "resources/css/about/styles.css";
-
-		$this->displaylibrary->display_page('about', $navbar, $data);
-	}
-
 }
