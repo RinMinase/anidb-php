6ff65d16f0b2e2766eb751a661a4e21057a44287
Rin Minase <rin.black.raison.detre@gmail.com>
2017-10-01 23:51:43 +0800

    ANIDB: modified routes for new controllers created

--- application/config/routes.php
+++ application/config/routes.php
@@ -17,11 +17,11 @@ $route['addEntry'] = 'DefaultController/addEntry';
 $route['last-watch'] = 'PageController/last_watch';
 $route['by-name'] = 'PageController/by_name';
 
-$route['download-list'] = 'PageController/download_list';
-$route['download-list/add'] = 'PageController/download_list_add';
-$route['download-list/(:num)/(:any)'] = 'PageController/download_list/$1/$2';
+$route['download-list'] = 'DownloadListController/download_list';
+$route['download-list/add'] = 'DownloadListController/download_list_add';
+$route['download-list/(:num)/(:any)'] = 'DownloadListController/download_list/$1/$2';
 
-$route['hdd-list'] = 'PageController/hdd_list';
-$route['hdd-list/simulator'] = 'PageController/hdd_simulator';
+$route['hdd-list'] = 'HDDListController/hdd_list';
+$route['hdd-list/simulator'] = 'HDDListController/hdd_simulator';
 
 $route['about'] = 'PageController/about';
