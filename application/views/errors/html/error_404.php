bf2a7caacbd3327914b2c1ba498947431ec7a57e
Rin Minase <rin.black.raison.detre@gmail.com>
2017-10-10 05:23:30 +0800

    FILE_SYSTEM: removed unused css code, cleaned css formatting, renamed classes for code clarity, removed unused classes, reduced code length (180 lines to 116 lines)

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -17,129 +17,66 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<title>404 Page Not Found</title>
 
 		<style>
-			html,body{
-				text-align:left;
-				font-size:1em
+			body { margin: 0px; }
+
+			h1, h2, p {
+				font-family: "Open Sans","Myriad Pro",Arial,sans-serif;
+				text-align: center;
+				color: #001c20;
+				margin: 0px;
 			}
 
-			html,body,img,form,textarea,input,fieldset,div,p,div,ul,li,ol,dl,dt,dd,h1,h2,h3,h4,h5,h6,pre,code{
-				margin:0;
-				padding:0
+			h1 { font-size: 1.7em; }
+
+			h2 {
+				font-weight: 300;
+				font-size: 1.5em;
 			}
 
-			ul,li{
-				list-style:none
+			p {
+				font-size: 1.25em;
+				padding-top: 2em;
 			}
 
-			img{
-				display:block
+			a {
+				color: #118798;
+				text-decoration: none;
 			}
 
-			a img{
-				border:0
-			}
+			a:hover { color: #B1EEF7 }
 
-			a{
-				text-decoration:none;
-				font-weight:normal;
-				font-family:inherit
-			}
-
-		 *:active,*:focus{
-				outline:0;
-				-moz-outline-style:none
-		 }
-
-		 h1,h2,h3,h4,h5,h6,h7{
-				font-weight:normal;
-				font-size:1em
-		 }
-
-		 .clearfix:after{
-				clear:both;
-				content:".";
-				display:block;
-				font-size:0;
-				height:0;
-				line-height:0;
-				visibility:hidden
-		 }
-
-		 .fourohfour .ocean{
+		 .container .header {
 				background-image: url(
 					<?php
 						echo "http://" . $_SERVER['SERVER_NAME'] . "/anidb/";
 						echo "resources/images/404/ocean.png";
 					?>
 				);
-				background-color: #0c8da0;
+				background-color: #0C8DA0;
 				background-repeat: no-repeat;
 				background-position: center 282px;
-				height:315px
+				height: 315px;
 			}
 
-			.fourohfour .ocean img{
-				margin-right:auto;
-				margin-left:auto
+			.container .header img {
+				margin: 0px auto;
 			}
 
-			.fourohfour .waves{
-				display:block;
-				padding-top:25px;
-				margin-right:auto;
-				margin-left:auto
+			.container .main {
+				display: block;
+				margin-top: 90px;
 			}
 
-			.fourohfour .main{
-				display:block;
-				margin-top:90px
+			.container .brand {
+				width: 150px;
+				margin-top: 3.5em;
+				margin-left: auto;
+				margin-right: auto;
 			}
 
-			.fourohfour .logo{
-				width:150px;
-				margin-top:3.5em;
-				margin-left:auto;
-				margin-right:auto
-			}
-
-			.fourohfour .fishy{
-				display:block;
-				padding-top:27px
-			}
-
-			.fourohfour .help{
-				padding-top:2em
-			}
-
-			.fourohfour h1{
-				font-family:"Open Sans","Myriad Pro",Arial,sans-serif;
-				font-weight:bold;
-				font-size:1.7em;
-				color:#001c20;
-				text-align:center
-			}
-
-			.fourohfour h2{
-				font-family:"Open Sans","Myriad Pro",Arial,sans-serif;
-				font-weight:300;
-				font-size:1.5em;
-				color:#001c20;
-				text-align:center
-			}
-
-			.fourohfour p{
-				font-family:"Open Sans","Myriad Pro",Arial,sans-serif;
-				font-size:1.25em;
-				color:#001c20;
-				text-align:center
-			}
-
-			.fourohfour a{
-				color:#118798
-			}
-
-			.fourohfour a:hover{
-				color:#b1eef7
+			.container .logo {
+				display: block;
+				padding-top: 27px;
 			}
 
 		</style>
@@ -147,9 +84,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</head>
 
 	<body>
-		<div class="fourohfour">
-			<div class="ocean">
-				<img alt="fish" class="fishy" src="
+		<div class="container">
+			<div class="header">
+				<img class="logo" src="
 					<?php
 						echo "http://" . $_SERVER['SERVER_NAME'] . "/anidb/";
 						echo "resources/images/404/404_logo.png";
@@ -160,13 +97,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div class="main">
 				<h1>Something's fishy here.</h1>
 				<h2>The page you requested does not exist.</h2>
-				<p class="help">
+				<p>
 					<a href="https://www.google.com/search?q=404+error">Why</a> might this be happening?
 				</p>
 			</div>
 
-			<div class="logo">
-				<img alt="logo" src="
+			<div class="brand">
+				<img src="
 					<?php
 						echo "http://" . $_SERVER['SERVER_NAME'] . "/anidb/";
 						echo "resources/images/404/brand.png";
@@ -176,4 +113,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		</div>
 	</body>
 </html>
-
