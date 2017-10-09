36945fd1f3292057c3ef588f491c1920bdf7457d
Rin Minase <rin.black.raison.detre@gmail.com>
2017-10-10 03:51:43 +0800

    FILE_SYSTEM: added line breaks and commented headings for code clarity

--- application/config/routes.php
+++ application/config/routes.php
@@ -5,6 +5,9 @@ $route['default_controller'] = 'LandingPageController';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
+
+/* LANDING PAGE */
+
 $route['view/(:num)'] = 'LandingPageController/view/$1';
 // $route['edit/(:num)'] = 'LandingPageController/edit/$1';
 // $route['delete/(:num)'] = 'LandingPageController/delete/$1';
@@ -14,18 +17,31 @@ $route['index'] = 'LandingPageController/index';
 $route['add'] = 'LandingPageController/add';
 $route['addEntry'] = 'LandingPageController/addEntry';
 
+
+/* OTHER LISTS */
+
 $route['last-watch'] = 'OtherListsController/last_watch';
 $route['by-name'] = 'OtherListsController/by_name';
 $route['summer-list'] = 'OtherListsController/summer_list';
+
+
+/* DOWNLOAD LISTS */
 
 $route['download-list'] = 'DownloadListController/download_list';
 $route['download-list/add'] = 'DownloadListController/download_list_add';
 $route['download-list/(:num)/(:any)'] = 'DownloadListController/download_list/$1/$2';
 
+
+/* HDD LISTS */
+
 $route['hdd-list'] = 'HDDListController/hdd_list';
 $route['hdd-list/simulator'] = 'HDDListController/hdd_simulator';
 
+
+/* GENERAL PAGES */
+
 $route['about'] = 'GeneralPagesController/about';
+
 
 /* Disable direct access to any controller */
 $route['DownloadListController/(:any)'] = 'GeneralPagesController/error_404';
