209bf27e2aa76eaa1118e5d964f6aadc6a84a754
Rin Minase <rin.black.raison.detre@gmail.com>
2017-10-10 05:28:13 +0800

    FILE_SYSTEM: added page scaling in 404 page for mobile or smaller devices

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -6,6 +6,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
 		<style>
 
