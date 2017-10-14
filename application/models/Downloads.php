40ddd6d6c2b2e698f205aec841bba3a0b5c2b706
Rin Minase <rin.black.raison.detre@gmail.com>
2017-10-15 06:47:00 +0800

    MDL_DOWNLOADS: moved model code from 'defaultmodel'

--- application/models/Downloads.php
+++ application/models/Downloads.php
@@ -2,5 +2,26 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Downloads extends CI_Model {
+	public function getDownloadData($year = NULL, $season = NULL) {
+		if (is_null($year) && is_null($season)) {
+			$this->db->select('status, season, year');
+			$this->db->from('downloads');
 
+			$this->db->order_by('year', 'DESC');
+			$this->db->order_by('season', 'ASC');
+			$this->db->order_by('status', 'DESC');
+		} else {
+			$this->db->select('id, title, status, priority, remarks');
+			$this->db->from('downloads');
+
+			$this->db->where('year', $year);
+			$this->db->where('season', $season);
+
+			$this->db->order_by('status', 'DESC');
+			$this->db->order_by('priority', 'DESC');
+			$this->db->order_by('title', 'ASC');
+		}
+
+		return $this->db->get()->result();
+	}
 }
