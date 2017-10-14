0023947fff27c6f1aa116ce1e16efc4c7f584048
Rin Minase <rin.black.raison.detre@gmail.com>
2017-10-15 07:10:46 +0800

    LIB_DISPLAY: renamed to 'display' from 'displaylibrary', changed autoload library name

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $autoload['packages'] = array();
-$autoload['libraries'] = array('database', 'displaylibrary');
+$autoload['libraries'] = array('database', 'display');
 $autoload['drivers'] = array();
 $autoload['helper'] = array('form', 'url');
 $autoload['config'] = array();
--- application/libraries/Display.php
+++ application/libraries/Display.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class DisplayLibrary {
+class Display {
 
 	public function display_page(
 		$page,
