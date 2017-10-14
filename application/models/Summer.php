167b9ddb8c44ce6f087bc53a11306eda75fc9eba
Rin Minase <rin.black.raison.detre@gmail.com>
2017-10-15 06:47:17 +0800

    MDL_SUMMER: moved model code from 'defaultmodel'

--- application/models/Summer.php
+++ application/models/Summer.php
@@ -2,5 +2,19 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Summer extends CI_Model {
+	public function getSummerData($id = NULL) {
+		if (empty($id)) {
+			$this->db->select('id, listTitle');
+			$this->db->from('summer');
 
+			return $this->db->get()->result();
+		} else {
+			$this->db->select('dateStart, dateEnd');
+			$this->db->from('summer');
+			$this->db->where('id', $id);
+			$this->db->order_by('listTitle', 'ASC');
+
+			return $this->db->get()->row();
+		}
+	}
 }
