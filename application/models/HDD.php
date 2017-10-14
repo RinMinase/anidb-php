00700dc56ba2bb3c4df801aaf9346bc675aec534
Rin Minase <rin.black.raison.detre@gmail.com>
2017-10-15 06:47:09 +0800

    MDL_HDD: moved model code from 'defaultmodel'

--- application/models/HDD.php
+++ application/models/HDD.php
@@ -2,5 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class HDD extends CI_Model {
+	public function getHDDData() {
+		$this->db->select('from, to, hddSize');
+		$this->db->from('hdd');
 
+		return $this->db->get()->result();
+	}
 }
