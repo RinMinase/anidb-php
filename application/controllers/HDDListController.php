6deb423fab9916afcbb3697c7ce863f6496f9750
Rin Minase <rin.black.raison.detre@gmail.com>
2017-10-18 03:14:09 +0800

    HDD_SIMULATOR: functional loading of default values

--- application/controllers/HDDListController.php
+++ application/controllers/HDDListController.php
@@ -32,11 +32,32 @@ class HDDListController extends CI_Controller {
 	}
 
 	public function hdd_simulator() {
+		$data['hddData'] = $this->hdd->getHDDData();
+
+		foreach ($data['hddData'] as $item) {
+			$temp['animeData'] = $this->anime->getAnimeDataByLetterRange($item->from, $item->to);
+			$temp['count'] = count($temp['animeData']);
+
+			$temp['filesize'] = 0;
+			foreach ($temp['animeData'] as $subitem) {
+				$temp['filesize'] += $subitem->filesize;
+			}
+
+			$temp['to'] = strtoupper($item->to);
+			$temp['from'] = strtoupper($item->from);
+			$temp['percent'] = round( ($temp['filesize'] / $item->hddSize) * 100  ,1);
+			$temp['free'] = round( ($item->hddSize - $temp['filesize']) / 1073741824, 2);
+			$temp['used'] = round($temp['filesize'] / 1073741824, 2);
+			$temp['total'] = round($item->hddSize / 1073741824, 2);
+
+			$data['animeByHDD'][] = $temp;
+		}
+
 		$navbar['activePage'] = "hdd-simulator";
 		$navbar['customTitle'] = "Disk Simulator";
 		$navbar['customCSS'] = "resources/css/hdd-simulator/styles.css";
 
-		$this->display->display_page('hdd-list/hdd-simulator', $navbar);
+		$this->display->display_page('hdd-list/hdd-simulator', $navbar, $data);
 	}
 
 }
--- application/views/hdd-list/hdd-simulator.php
+++ application/views/hdd-list/hdd-simulator.php
@@ -51,38 +51,34 @@
 					<div class="row">
 						<div class="col-md-12">
 
-							<div class="row">
-								<div class="col-xs-2 col-sm-2 col-md-4 col-lg-4">
-									<p><strong>A - I</strong></p>
-								</div>
-								<div class="col-xs-10 col-sm-10 col-md-8 col-lg-8">
-									<div class="progress">
-										<div class="progress-bar progress-bar-success" style="width:50%">50%</div>
+							<?php foreach ($animeByHDD as $item): ?>
+								<div class="row">
+									<div class="col-xs-2 col-sm-2 col-md-4 col-lg-4">
+										<p><strong><?php echo $item['from'] ?> - <?php echo $item['to'] ?></strong></p>
 									</div>
-								</div>
-							</div>
+									<div class="col-xs-10 col-sm-10 col-md-8 col-lg-8">
+										<div class="progress"
+											tooltip="tip"
+											title="<?php
+												echo "Free : " . $item['free'] . " GB\n";
+												echo "Used : " . $item['used'] . " GB\n";
+												echo "Total : " . $item['total'] . " GB\n";
+												echo "Titles : " . $item['count'];
+											?>">
+											<div class=
+												<?php
+													if ($item['percent'] >= 0 && $item['percent'] < 80) echo "'progress-bar progress-bar-success'";
+													else if ($item['percent'] >= 80 && $item['percent'] < 90) echo "'progress-bar progress-bar-warning'";
+													else echo "'progress-bar progress-bar-danger'";
+												?>
+												style="width:<?php echo round($item['percent'], 0); ?>%">
 
-							<div class="row">
-								<div class="col-xs-2 col-sm-2 col-md-4 col-lg-4">
-									<p><strong>J - Q</strong></p>
-								</div>
-								<div class="col-xs-10 col-sm-10 col-md-8 col-lg-8">
-									<div class="progress">
-										<div class="progress-bar progress-bar-warning" style="width:80%">80%</div>
+												<?php echo $item['percent'] . "%"; ?>
+											</div>
+										</div>
 									</div>
 								</div>
-							</div>
-
-							<div class="row">
-								<div class="col-xs-2 col-sm-2 col-md-4 col-lg-4">
-									<p><strong>R - Z</strong></p>
-								</div>
-								<div class="col-xs-10 col-sm-10 col-md-8 col-lg-8">
-									<div class="progress">
-										<div class="progress-bar progress-bar-danger" style="width:90%">90%</div>
-									</div>
-								</div>
-							</div>
+							<?php endforeach; ?>
 
 						</div>
 					</div>
@@ -95,107 +91,81 @@
 		<div class="col-md-9">
 			<div class="panel-group">
 
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h4 class="panel-title">
-							<a href="#panel-ai" data-toggle="collapse">A - I</a>
-							<div class="pull-right">
-								<span class="badge">75</span>
-								<span>&nbsp;00.00 GB</span>
-							</div>
-						</h4>
-					</div>
-					<div id="panel-ai" class="panel-collapse collapse">
-						<div class="panel-body">
+				<?php foreach ($animeByHDD as $item): ?>
 
-							<div class="table-responsive">
-								<table class="table">
-									<thead>
-										<tr>
-											<th>Title</th>
-											<th>File Size</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<td>Sample</td>
-											<td>Sample</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title">
+								<a href="<?php echo "#panel-" . $item['from'] . $item['to'] ?>" data-toggle="collapse">
+									<?php echo $item['from'] ?> - <?php echo $item['to'] ?>
+								</a>
+								<div class="pull-right">
+									<span class="badge"><?php echo $item['count']; ?></span>
+									<span>&nbsp;<?php echo round($item['filesize'] / 1073741824, 2); ?> GB</span>
+								</div>
+							</h4>
+						</div>
+						<div id="<?php echo "panel-" . $item['from'] . $item['to'] ?>" class="panel-collapse collapse">
+							<div class="panel-body">
 
+								<div class="table-responsive">
+									<table class="table table-condensed">
+										<thead>
+											<tr>
+												<th>Title</th>
+												<th>File Size</th>
+											</tr>
+										</thead>
+										<tbody>
+
+											<?php foreach ($item['animeData'] as $subitem): ?>
+												<tr>
+													<td>
+														<?php
+															switch ($subitem->quality) {
+																case "4K 2160p":
+																	echo "<div class='anime-legend-uhd' tooltip='tip' title='4K 2160p'></div>";
+																break;
+																case "FHD 1080p":
+																	echo "<div class='anime-legend-fhd' tooltip='tip' title='FHD 1080p'></div>";
+																break;
+																case "HD 720p":
+																	echo "<div class='anime-legend-hd' tooltip='tip' title='HD 720p'></div>";
+																break;
+																case "HQ 480p":
+																	echo "<div class='anime-legend-hq' tooltip='tip' title='HQ 480p'></div>";
+																break;
+																case "LQ 360p":
+																	echo "<div class='anime-legend-lq' tooltip='tip' title='LQ 360p'></div>";
+																break;
+															}
+														?>
+
+														<span><?php echo $subitem->title; ?></span>
+													</td>
+													<td>
+														<?php
+															if ($subitem->filesize == 0) {
+																echo "-";
+															} else if ($subitem->filesize < 1073741824) {
+																echo round($subitem->filesize / 1048576, 2) . " MB";
+															} else {
+																echo round($subitem->filesize / 1073741824, 2) . " GB";
+															}
+														?>
+													</td>
+												</tr>
+											<?php endforeach; ?>
+
+										</tbody>
+									</table>
+								</div>
+
+							</div>
 						</div>
 					</div>
-				</div>
 
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h4 class="panel-title">
-							<a href="#panel-jq" data-toggle="collapse">J - Q</a>
-							<div class="pull-right">
-								<span class="badge">92</span>
-								<span>&nbsp;00.00 GB</span>
-							</div>
-						</h4>
-					</div>
-					<div id="panel-jq" class="panel-collapse collapse">
-						<div class="panel-body">
-
-							<div class="table-responsive">
-								<table class="table table-condensed">
-									<thead>
-										<tr>
-											<th>Title</th>
-											<th>File Size</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<td>Sample</td>
-											<td>Sample</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-
-						</div>
-					</div>
-				</div>
-
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h4 class="panel-title">
-							<a href="#panel-rz" data-toggle="collapse">R - Z</a>
-							<div class="pull-right">
-								<span class="badge">107</span>
-								<span>&nbsp;00.00 GB</span>
-							</div>
-						</h4>
-					</div>
-					<div id="panel-rz" class="panel-collapse collapse">
-						<div class="panel-body">
-
-							<div class="table-responsive">
-								<table class="table table-condensed">
-									<thead>
-										<tr>
-											<th>Title</th>
-											<th>File Size</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<td>Sample</td>
-											<td>Sample</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-
-						</div>
-					</div>
-				</div>
+				<?php endforeach; ?>
 
 			</div>
 		</div>
