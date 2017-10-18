4ac670f0a5a3d5d6ef17492efcd9c9fd67dbc13b
Rin Minase <rin.black.raison.detre@gmail.com>
2017-10-18 23:08:01 +0800

    HDD_SIMULATOR: partial functional adding of hdd  and rearranging hdd accordingly

--- application/controllers/HDDListController.php
+++ application/controllers/HDDListController.php
@@ -36,22 +36,13 @@ class HDDListController extends CI_Controller {
 			$data['hddData'] = $this->hdd->getHDDData();
 		} else {
 			$oldData = $this->input->post('oldData');
-			$newFrom = $this->input->post('from');
-			$newTo = $this->input->post('to');
+			$newFrom = strtolower($this->input->post('from'));
+			$newTo = strtolower($this->input->post('to'));
 
-			$newData = ',{"from":"' . $newFrom . '","to":"' . $newTo . '","hddSize":"1000169533440"}';
-			$splicedOldData = substr($oldData, 1, -1);
-			$mergedData = "[" . $splicedOldData . $newData . "]";
-
-			$data['hddData'] = json_decode($mergedData);
-
-			usort($data['hddData'], function ($a, $b) {
-				return ord($a->from) > ord($b->from);
-			});
-
+			$data['hddData'] = $this->_rearrangeHDD($oldData, $newFrom, $newTo);
 		}
 
-		// $this->display->print_pretty($data['hddData']);
+		$this->display->print_pretty($data['hddData']);
 
 		foreach ($data['hddData'] as $item) {
 			$temp['animeData'] = $this->anime->getAnimeDataByLetterRange($item->from, $item->to);
@@ -79,4 +70,40 @@ class HDDListController extends CI_Controller {
 		$this->display->display_page('hdd-list/hdd-simulator', $navbar, $data);
 	}
 
+	private function _rearrangeHDD($oldData, $newFrom, $newTo) {
+		$newData = '{"from":"' . $newFrom . '","to":"' . $newTo . '","hddSize":"1000169533440"},';
+		$splicedOldData = substr($oldData, 1, -1);
+		$mergedData = "[" . $newData . $splicedOldData . "]";
+
+		$hddData = json_decode($mergedData);
+
+		for ($i=1; $i < 27; $i++) {
+			$letters[$i] = -1;
+		}
+
+		for ($hdd = 0; $hdd < count($hddData); $hdd++) {
+			for ($char = ord($hddData[$hdd]->from) - 96; $char <= ord($hddData[$hdd]->to) - 96; $char++) {
+				if ($letters[$char] == -1) {
+					$letters[$char] = $hdd;
+				}
+			}
+		}
+
+		$oldLetter = $letters[1];
+		for ($i=1; $i < 27; $i++) {
+			if($letters[$i] != -1) {
+				if($letters[$i] != $oldLetter) {
+					$hddData[$letters[$i-1]]->to = chr($i - 1 + 96);
+					$hddData[$letters[$i]]->from = chr($i + 96);
+					$oldLetter = $letters[$i];
+				}
+			}
+		}
+
+		usort($hddData, function ($a, $b) {
+			return ord($a->from) > ord($b->from);
+		});
+
+		return $hddData;
+	}
 }
