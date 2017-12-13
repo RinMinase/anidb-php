91ee9eb6609f76b898c8d53014a01599bcb4c1b2
Rin Minase <rin.black.raison.detre@gmail.com>
2017-12-14 05:08:16 +0800

    LANDING_ADD: added autocomplete for filesize data

--- resources/js/add/scripts.js
+++ resources/js/add/scripts.js
@@ -1,11 +1,20 @@
+var filesize = 0;
+
 Dropzone.options.dropzone = {
 	paramName: "file",
 	maxFilesize: 20480, // 20 GB (20480 MB)
 	addRemoveLinks: true,
-	parallelUploads: 30,
+	parallelUploads: 32,
 	uploadMultiple: true,
 
 	init: function() {
+
+		this.on("addedfile", function(file) {
+			filesize += file.size;
+
+			/** FILESIZE */
+			$('input[name=filesize]').val(filesize);
+		});
 
 		this.on("sending", function(file) {
 			this.removeFile(file);
